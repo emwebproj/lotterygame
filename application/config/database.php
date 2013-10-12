eb64b5c3c180f5beefebbaf1572e5fab3ef58d62
emwebproj <em.webproj@gmail.com>
2013-10-12 10:46:43 +0800

    Changes

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'tunnel.pagodabox.com:3306';
+$db['default']['hostname'] = 'tunnel.pagodabox.com';
 $db['default']['username'] = 'joi';
 $db['default']['password'] = 'Xi2Wydgc';
 $db['default']['database'] = 'lottery';
