bdba8a1369c831cbfcb632720a793b498f1467b5
emwebproj <em.webproj@gmail.com>
2013-10-12 10:53:58 +0800

    Changes

--- application/config/database.php
+++ application/config/database.php
@@ -63,6 +63,8 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+$db['default']['port']     = 3306;
+
 
 
 /* End of file database.php */
